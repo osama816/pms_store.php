73ec0cd7d01ce14b25088b729dd19900193e29e9
osama Elgendy <149249878+osama816@users.noreply.github.com>
2025-04-05 19:50:03 +0200

    Update add_product.php

--- handelers/add_product.php
+++ handelers/add_product.php
@@ -2,12 +2,9 @@
 require_once "../core/function.php";
 require_once "../core/validete.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $price = $_POST['price'];
-    $price_befor_discount = $_POST['price_befor_discount'];
-    $product_name = $_POST['product_name'];
-    $img_path = $_POST['img_path'];
-    $details = $_POST['details'];
-    $type = $_POST['type'];
+    foreach ($_POST as $key => $value) {
+        $$key = htmlspecialchars(trim($value));
+    }
 }
 
 if (addproduct($product_name, $price, $img_path, $price_befor_discount, $details, $type)) {
