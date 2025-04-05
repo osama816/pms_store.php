353328b73f1aff7f80a11fda02573247dd5e0389
osama elgendy <elgendyo240.com>
2025-04-05 19:44:25 +0200

    edit validattion

--- handelers/deletproduct_cart.php
+++ handelers/deletproduct_cart.php
@@ -1,14 +1,11 @@
 <?php
 include "../core/function.php";
 if ($_SERVER["REQUEST_METHOD"] !== "POST" || empty($_POST["id"]) || !isset($_POST["id"])) {
-    foreach ($_POST as $key => $value) {
-        $$key = htmlspecialchars(trim($value));
-    }
     setmessage("danger", " invalid product ");
     header('Location:../cart.php ');
     exit;
 }
-
+ $id =$_POST["id"] ;
 
 if (deletproduct_cart($id)) {
     setmessage("success", "delet product success");
